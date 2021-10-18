33fa55938c3a6fd2be9d24bb767bbcf76eb280ef
Matheus Oliveira <matheussouzapoliveira@gmail.com>
2021-10-17 22:23:45 -0300

    ajustes

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 int main(int argc, char *argv[]) {
   Vetor* vetores[3];
 
-  vetores[0] = criaVetor("V1", 100000);
-  vetores[1] = criaVetor("V3", 100000);
-  vetores[2] = criaVetor("V3", 100000);
+  vetores[0] = criaVetor("V1", 5);
+  vetores[1] = criaVetor("V3", 5);
+  vetores[2] = criaVetor("V3", 5);
 
   printf("Merging repeticao\n");
   mergingRepeticao(vetores, 3);
   
-  // printf("Merging\n");
-  // merging(vetores, 3);
+  printf("Merging\n");
+  merging(vetores, 3);
 
-  // printf("Matching\n");
-  // matching(vetores, 3);
+  printf("Matching\n");
+  matching(vetores, 3);
   
   return 0;
 }
